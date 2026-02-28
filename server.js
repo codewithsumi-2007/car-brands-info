a2b560e12b9f836839d42ab80ee7bbff88187f63
codewithsumi-2007 <sumitkwn@gmail.com>
2026-02-28 21:03:02 +0530

    Set up Express backend in server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Car Brands API!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
